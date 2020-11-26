bdeb0bb4753ece6600fabf1b5a53391ad124abd4
CyberWolf37 <borel.loic.37@gmail.com>
2020-11-26 09:12:40 +0100

    to rebuild heroku

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     let mut bot = BotMessenger::new();
     bot.get_conf_mut().set_token_fb_page(&std::env::var("TOKEN_FB").unwrap_or("MamaGuriba".to_string()));
     bot.get_conf_mut().set_token_webhook(&std::env::var("TOKEN").unwrap_or("MamaGuriba".to_string()));
+    
     let mut block = Block::new("Hello");
         block.add(Arc::new(CartBox::new(Arc::new(|x: &BotUser| {
            let mes = Message::new("Hello mother fucker");
