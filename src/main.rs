96ad8c78f4b4a66f78d9ded3feb682596bec7533
CyberWolf37 <borel.loic.37@gmail.com>
2020-11-26 09:31:49 +0100

    set port env

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let mut bot = BotMessenger::new();
     bot.get_conf_mut().set_token_fb_page(&std::env::var("TOKEN_FB").unwrap_or("MamaGuriba".to_string()));
     bot.get_conf_mut().set_token_webhook(&std::env::var("TOKEN").unwrap_or("MamaGuriba".to_string()));
-    
+    bot.get_conf_mut().set_port(std::env::var("PORT").unwrap_or("7878".to_string()).parse::<u16>().unwrap_or(7878));
+
     let mut block = Block::new("Hello");
         block.add(Arc::new(CartBox::new(Arc::new(|x: &BotUser| {
            let mes = Message::new("Hello mother fucker");
