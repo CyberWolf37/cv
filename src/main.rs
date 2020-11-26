9e196bfec4289c3d57c47b9ec37283c3922c6b94
CyberWolf37 <borel.loic.37@gmail.com>
2020-11-26 10:14:06 +0100

    Set block configuration

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,16 @@ fn main() {
     bot.get_conf_mut().set_token_webhook(&std::env::var("TOKEN").unwrap_or("MamaGuriba".to_string()));
     bot.get_conf_mut().set_port(std::env::var("PORT").unwrap_or("7878".to_string()).parse::<u16>().unwrap_or(7878));
 
+    let message = Message::new(bot.get_conf().get_token_webhook());
+    let message_1 = Message::new(bot.get_conf().get_token_webhook());
+
     let mut block = Block::new("Hello");
-        block.add(Arc::new(CartBox::new(Arc::new(|x: &BotUser| {
-           let mes = Message::new("Hello mother fucker");
-           mes.send(x);
+        block.add(Arc::new(CartBox::new(Arc::new(move |x: &BotUser| {
+            message.send(x,"Hello mother fucker");
         }))));
     let block_start = Block::new("#start");
-        block.add(Arc::new(CartBox::new(Arc::new(|x: &BotUser| {
-           let mes = Message::new("Start cv");
-           mes.send(x);
+        block.add(Arc::new(CartBox::new(Arc::new(move |x: &BotUser| {
+            message_1.send(x,"Start cv");
         }))));
         bot.add_block(block)
            .add_block(block_start);
