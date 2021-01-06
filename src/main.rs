419cf14ea95f3b711f3e6c82dfa24d63cda5419f
CyberWolf37 <borel.loic.37@gmail.com>
2021-01-06 21:02:27 +0100

    Ajout block for cv

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use utils::block::Block;
 use utils::block::CartBox;
 use api::card::Card;
 use api::card::CardGeneric;
+use api::card::CardButton;
 use api::button::Button;
 
 fn main() {
@@ -11,6 +12,26 @@ fn main() {
     let TOKEN_FB = &std::env::var("TOKEN_FB").unwrap_or("MamaGuriba".to_string());
     let TOKEN_WH = &std::env::var("TOKEN").unwrap_or("MamaGuriba".to_string());
     let PORT = std::env::var("PORT").unwrap_or("7878".to_string()).parse::<u16>().unwrap_or(7878);
+
+    // BLOCKS
+    let block_hello = Block::new("#Start")
+                        .cartBox(CartBox::new()
+                            .text("Bonjour 😃 comment vas-tu ?
+                                \nmoi j'ai la pêche 🎣
+                                \nVeut-tu que je te détail mon CV 📖 ?")
+                            .button_postback("Oui 👍", "#CvStart")
+                            .button_postback("Non 👎", "#Nop"));
+
+    let block_nop = Block::new("#Nop")
+                        .cartBox(CartBox::new()
+                            .text("Dommage je sentais une harmony 🖖 entre nous"));
+
+    let block_cv = Block::new("#CvStart")
+                        .cartBox(CartBox::new()
+                            .card(CardButton::new()
+                                .button(Button::new_button_pb("Sports", "#Sports"))
+                                .button(Button::new_button_pb("Cursus", "#Cursus")
+                                .button(Button::new_button_pb("Hobbies", "#Hobbies")))));
 
     BotMessenger::new()
             .block(Block::new("Hello")
